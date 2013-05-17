0157af513d2760ce211402651329084a9d07bb75
Toni Petrina <tonipetrina@gmail.com>
2013-05-17 14:58:18 +0200

    Added sample for Stack Overflow topic

---
+++ BlogResearch/NavigateToLibraryPage/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace NavigateToLibraryPage
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/wp7.library;component/Page.xaml", UriKind.Relative));
+        }
+    }
+}

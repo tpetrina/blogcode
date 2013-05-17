ebf754d748bebadb28628e244ab9525433a0d810
Toni Petrina <tonipetrina@gmail.com>
2013-05-17 14:40:28 +0200

    Added sample project for the Viewbox control

---
+++ BlogResearch/ViewboxTesting/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace ViewboxTesting
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public string FullName
+        {
+            get { return "At the\r\nworld's end"; }
+        }
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ BlogResearch/ViewboxTesting/MainPage2.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace ViewboxTesting
+{
+    public partial class MainPage2 : PhoneApplicationPage
+    {
+        public string FullName
+        {
+            get { return "At the\r\nworld's end"; }
+        }
+
+        // Constructor
+        public MainPage2()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}

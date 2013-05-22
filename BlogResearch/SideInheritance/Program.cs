b585d69a111efe0608fb91ff8af7c90109c3a549
Toni Petrina <tonipetrina@gmail.com>
2013-05-22 20:58:30 +0200

    Adding sample for side inheritance

---
+++ BlogResearch/SideInheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideInheritance
+{
+    interface IFoo
+    {
+        void foo();
+    }
+
+    class Foo
+    {
+        public void foo()
+        {
+
+        }
+    }
+
+    internal class FooWithInterface : Foo, IFoo
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new FooWithInterface();
+            f.foo();
+        }
+    }
+}

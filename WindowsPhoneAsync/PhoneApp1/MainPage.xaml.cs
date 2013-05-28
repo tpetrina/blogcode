ed6a7a3262942efa973e30c57e6e9fe65c2920ce
Toni Petrina <tonipetrina@gmail.com>
2013-05-29 00:54:26 +0200

    Adding testing for async and WP71

---
+++ WindowsPhoneAsync/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace PhoneApp1
+{
+    public partial class MainPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async Task FooAsync()
+        {
+            await TaskEx.Delay(1000);
+        }
+    }
+}
---
+++ WindowsPhoneAsync/SlXnaApp1/SlXnaApp1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SlXnaApp1
+{
+    public partial class MainPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Simple button Click event handler to take us to the second page
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
+        }
+
+        public async Task FooAsync()
+        {
+            await TaskEx.Delay(1000);
+        }
+    }
+}

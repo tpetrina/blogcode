0f53f13c004a27a76fedc79f32cb6d0905c4c1ec
Toni Petrina <tonipetrina@gmail.com>
2013-05-30 12:36:48 +0200

    Adding sample for the blog post

---
+++ BlogResearch/ContentControlDataTemplate/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace ContentControlDataTemplate
+{
+    public class SomeViewModel
+    {
+        public string Data { get; set; }
+
+        public SomeViewModel()
+        {
+            Data = "Hello";
+        }
+    }
+
+    public partial class MainPage
+    {
+        public SomeViewModel Some { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Some = new SomeViewModel();
+            DataContext = this;
+        }
+    }
+}

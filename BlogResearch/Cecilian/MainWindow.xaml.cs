299618bf7dbfbe7f1d60809d088a8b26822d37dd
Toni Petrina <toni.petrina@gmail.com>
2013-06-06 17:51:53 +0200

    Testing Mono.Cecil

---
+++ BlogResearch/Cecilian/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mono.Cecil;
+
+namespace Cecilian
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var assembly = AssemblyDefinition.CreateAssembly(new AssemblyNameDefinition("TestModule", new Version()), "TestModule.exe", ModuleKind.Console);
+
+            var entryType = new TypeDefinition("TestModule", "Program", TypeAttributes.Class | TypeAttributes.Public);
+            var mainMethod = new MethodDefinition("Main", MethodAttributes.Public | MethodAttributes.Static, assembly.MainModule.TypeSystem.Void);
+            entryType.Methods.Add(mainMethod);
+            assembly.MainModule.Types.Add(entryType);
+            assembly.Write("TestModule.exe");
+        }
+    }
+}

b9b2636b8ac10c5f20862a6d2682aef2830d65d4
Toni Petrina <tonipetrina@gmail.com>
2013-06-17 06:56:40 +0200

    Finishing portable libraries test

---
+++ BlogResearch/testapp.wp8/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using core;
+
+namespace testapp.wp8
+{
+    public partial class MainPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Utilities.ShowMessage("Hello from WP8");
+        }
+    }
+}

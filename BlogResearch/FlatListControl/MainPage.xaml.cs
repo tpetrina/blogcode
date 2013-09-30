69fd191b69216fdbc7064955192463160546368e
Toni Petrina <info@tonicodes.net>
2013-09-30 19:28:04 +0200

    Adding FlatListControl template

---
+++ BlogResearch/FlatListControl/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace FlatListControl
+{
+    public partial class MainPage
+    {
+        public class Node
+        {
+            public string Title { get; set; }
+            public List<Node> Children { get; set; }
+        }
+
+        public List<Node> MasterList { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterList = new List<Node>
+                {
+                    new Node
+                        {
+                            Title = "Item 1",
+                            Children = new List<Node>
+                                {
+                                    new Node { Title = "Subitem1" },
+                                    new Node { Title = "Subitem2" },
+                                    new Node { Title = "Subitem3" },
+                                    new Node { Title = "Subitem4" }
+                                }
+                        },
+                    new Node
+                        {
+                            Title = "Item 2",
+                            Children = new List<Node>
+                                {
+                                    new Node { Title = "Subitem1" },
+                                    new Node { Title = "Subitem2" },
+                                    new Node { Title = "Subitem3" },
+                                    new Node { Title = "Subitem4" }
+                                }
+                        },
+                    new Node
+                        {
+                            Title = "Item 3",
+                            Children = new List<Node>
+                                {
+                                    new Node { Title = "Subitem1" },
+                                    new Node { Title = "Subitem2" },
+                                    new Node { Title = "Subitem3" },
+                                    new Node { Title = "Subitem4" }
+                                }
+                        }
+                };
+
+            DataContext = this;
+        }
+    }
+}
